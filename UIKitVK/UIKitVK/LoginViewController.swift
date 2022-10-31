7413fc4a70d3ee371fc421d805b55c7653c5df12
Maxim Rozhkov <rojkov.m@rambler.ru>
2022-11-01 01:35:55 +0300

    констрейнты для 1ого экрана

---
+++ UIKitVK/UIKitVK/LoginViewController.swift
@@ -0,0 +1,11 @@
+// LoginViewController.swift
+// Copyright © RoadMap. All rights reserved.
+
+import UIKit
+
+/// Контроллер входа в приложение
+final class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
