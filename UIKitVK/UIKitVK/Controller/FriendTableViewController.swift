58f7d75e85affbede01faae5cbe2019768e47f1c
Maxim Rozhkov <rojkov.m@rambler.ru>
2022-11-02 17:12:44 +0300

    исправил замечания 2.0

--- UIKitVK/UIKitVK/Controller/FriendTableViewController.swift
+++ UIKitVK/UIKitVK/Controller/FriendTableViewController.swift
@@ -6,27 +6,27 @@ import UIKit
 /// Экран друзей
 final class FriendTableViewController: UITableViewController {
     // MARK: - Private Constants
-    
+
     private enum Constants {
         static let identifierFriendTableViewCellID = "FriendTableViewCell"
     }
-    
+
     // MARK: - Life Cycle
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
+
     // MARK: - UITableViewDataSource, UITableViewDelegate
-    
+
     override func numberOfSections(in tableView: UITableView) -> Int {
         1
     }
-    
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         1
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(
             withIdentifier: Constants.identifierFriendTableViewCellID,
@@ -34,7 +34,7 @@ final class FriendTableViewController: UITableViewController {
         ) as? FriendTableViewCell else { return UITableViewCell() }
         return cell
     }
-    
+
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         UITableView.automaticDimension
     }
--- UIKitVK/UIKitVK/Controller/LoginViewController.swift
+++ UIKitVK/UIKitVK/Controller/LoginViewController.swift
@@ -6,7 +6,7 @@ import UIKit
 /// Экран авторизации
 final class LoginViewController: UIViewController {
     // MARK: - Private Constants
-    
+
     private enum Constants {
         static let identifier = "singInSegue"
         static let loginText = "q"
@@ -15,40 +15,43 @@ final class LoginViewController: UIViewController {
         static let alertMessageText = "Введен неверный логин и/или пароль"
         static let alertActionTitleText = "OK"
     }
-    
+
     // MARK: - Private IBOutlet
-    
+
     @IBOutlet private var loginScrollView: UIScrollView!
     @IBOutlet private var loginTextField: UITextField!
     @IBOutlet private var passwordTextField: UITextField!
-    
+
     // MARK: - Life Cycle
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         keyboardAddObserver()
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         keyboardRemoveObserver()
     }
-    
+
     // MARK: - Public Methods
-    
+
     override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
         guard identifier == Constants.identifier,
-              checkLoginInfo() else {
-            showAlert(alertTitle: Constants.alertTitleText,
-                      alertMessage: Constants.alertMessageText,
-                      alertActionTitle: Constants.alertActionTitleText)
+              checkLoginInfo()
+        else {
+            showAlertController(
+                alertTitle: Constants.alertTitleText,
+                alertMessage: Constants.alertMessageText,
+                alertActionTitle: Constants.alertActionTitleText
+            )
             return false
         }
         return true
     }
-    
+
     // MARK: - Private Methods
-    
+
     private func keyboardAddObserver() {
         NotificationCenter.default.addObserver(
             self,
@@ -65,47 +68,33 @@ final class LoginViewController: UIViewController {
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboardAction))
         loginScrollView.addGestureRecognizer(tapGesture)
     }
-    
+
     private func keyboardRemoveObserver() {
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
     }
-    
+
     private func checkLoginInfo() -> Bool {
         guard loginTextField.text == Constants.loginText,
               loginTextField.text == Constants.passwordText else { return false }
         return true
     }
-    
+
     @objc private func keyboardWillShownAction(notification: Notification) {
         guard let info = notification.userInfo as? NSDictionary,
               let keyboardSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as? NSValue)?.cgRectValue
-            .size else { return }
+              .size else { return }
         let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardSize.height, right: 0.0)
         loginScrollView.contentInset = contentInsets
         loginScrollView.scrollIndicatorInsets = contentInsets
     }
-    
+
     @objc private func keyboardWillHideAction(notification: Notification) {
         loginScrollView.contentInset = UIEdgeInsets.zero
         loginScrollView.scrollIndicatorInsets = UIEdgeInsets.zero
     }
-    
+
     @objc private func hideKeyboardAction() {
         loginScrollView.endEditing(true)
-    }
-}
-
-// Расширение LoginViewController для вызова AlertController
-extension LoginViewController {
-    func showAlert(alertTitle: String?, alertMessage: String?, alertActionTitle: String?) {
-        let alertController = UIAlertController(
-            title: alertTitle,
-            message: alertMessage,
-            preferredStyle: .alert
-        )
-        let alertControllerAction = UIAlertAction(title: alertActionTitle, style: .cancel)
-        alertController.addAction(alertControllerAction)
-        present(alertController, animated: true)
     }
 }
