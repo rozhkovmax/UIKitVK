7559bc919f0c34508af69b385c010857933818d6
Maxim Rozhkov <rojkov.m@rambler.ru>
2022-11-04 02:11:11 +0300

    сделал ДЗ 3-4

--- UIKitVK/UIKitVK/LikeButton.swift
+++ UIKitVK/UIKitVK/LikeButton.swift
@@ -4,4 +4,31 @@
 import UIKit
 
 /// Кнопка лайков
-final class LikeButton: UIControl {}
+final class LikeButton: UIControl {
+    // MARK: - Private Constants
+
+    private enum Constants {
+        static let likeZero = "0"
+        static let likeOne = "1"
+        static let heartImageName = "heart"
+        static let heartFillImageName = "heart.fill"
+    }
+
+    // MARK: - Private IBOutlet
+
+    @IBOutlet private var likeButton: UIButton!
+
+    // MARK: - Private IBAction
+
+    @IBAction private func likeButtonClick(_ sender: UIButton) {
+        if likeButton.tag == 0 {
+            likeButton.setImage(UIImage(systemName: Constants.heartImageName), for: .normal)
+            likeButton.setTitle(Constants.likeZero, for: .normal)
+            likeButton.tag = 1
+        } else {
+            likeButton.setImage(UIImage(systemName: Constants.heartFillImageName), for: .normal)
+            likeButton.setTitle(Constants.likeOne, for: .normal)
+            likeButton.tag = 0
+        }
+    }
+}
--- UIKitVK/UIKitVK/View/FriendCollectionViewCell.swift
+++ UIKitVK/UIKitVK/View/FriendCollectionViewCell.swift
@@ -5,31 +5,7 @@ import UIKit
 
 /// Галерея друга
 final class FriendCollectionViewCell: UICollectionViewCell {
-    // MARK: - Private Constants
-
-    private enum Constants {
-        static let likeZero = "0"
-        static let likeOne = "1"
-        static let heartImageName = "heart"
-        static let heartFillImageName = "heart.fill"
-    }
-
     // MARK: - IBOutlet
 
     @IBOutlet var friendGalleryImageView: UIImageView!
-    @IBOutlet var likeButton: UIButton!
-
-    // MARK: - IBAction
-
-    @IBAction func likeButtonClick(_ sender: UIButton) {
-        if likeButton.tag == 0 {
-            likeButton.setImage(UIImage(systemName: Constants.heartImageName), for: .normal)
-            likeButton.setTitle(Constants.likeZero, for: .normal)
-            likeButton.tag = 1
-        } else {
-            likeButton.setImage(UIImage(systemName: Constants.heartFillImageName), for: .normal)
-            likeButton.setTitle(Constants.likeOne, for: .normal)
-            likeButton.tag = 0
-        }
-    }
 }
