0f37359576629141e6f2c41afbe16dd1681f6fb0
Maxim Rozhkov <rojkov.m@rambler.ru>
2022-11-09 00:30:47 +0300

    исправления

--- UIKitVK/UIKitVK/Controller/FriendTableViewController.swift
+++ UIKitVK/UIKitVK/Controller/FriendTableViewController.swift
@@ -6,24 +6,24 @@ import UIKit
 /// Экран друзей
 final class FriendTableViewController: UITableViewController {
     // MARK: - Private Properties
-
+    
     private let friends = vkFriends
     private var sections: [Character: [User]] = [:]
     private var sectionNameChar: [Character] = []
-
+    
     // MARK: - Life Cycle
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
     }
-
+    
     // MARK: - Private Methods
-
+    
     private func setupUI() {
         headerFriendName()
     }
-
+    
     private func headerFriendName() {
         for friendName in friends {
             guard let firstChar = friendName.friendName.first else { return }
@@ -35,42 +35,42 @@ final class FriendTableViewController: UITableViewController {
         }
         sectionNameChar = Array(sections.keys).sorted()
     }
-
+    
     // MARK: - UITableViewDataSource
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         guard let sectionCount = sections[sectionNameChar[section]]?.count else { return 0 }
         return sectionCount
     }
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         sections.count
     }
-
+    
     override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
         sectionNameChar.compactMap { String($0) }
     }
-
+    
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         String(sectionNameChar[section])
     }
-
+    
     override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
         tableView.backgroundColor = UIColor(named: Constants.CustomColorName.lightGrayCustomColorName)?
             .withAlphaComponent(0.95)
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(
             withIdentifier: Constants.Identifiers.identifierFriendTableViewCellID,
             for: indexPath
         ) as? FriendTableViewCell,
-            let friend = sections[sectionNameChar[indexPath.section]]?[indexPath.row]
+              let friend = sections[sectionNameChar[indexPath.section]]?[indexPath.row]
         else { return UITableViewCell() }
         cell.refreshFriend(friend)
         return cell
     }
-
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let storyboard = UIStoryboard(name: Constants.OtherConstants.storyboardName, bundle: nil)
         guard let friendCollectionVC = storyboard
